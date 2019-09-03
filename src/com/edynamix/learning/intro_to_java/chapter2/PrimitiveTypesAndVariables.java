f45c7d11dbed5cc18ae9394200e3ce1062d93f77
Bilyana Gospodinova <gospodinova_beki@mail.bg>
2019-09-03 21:06:02 +0300

    Chapter 1 progress.

---
+++ src/com/edynamix/learning/intro_to_java/chapter2/PrimitiveTypesAndVariables.java
@@ -0,0 +1,11 @@
+package com.edynamix.learning.intro_to_java.chapter2;
+
+public class PrimitiveTypesAndVariables {
+
+	public static void main(String[] args) {
+		//Assert 0.1f is rounded.
+		float sum = 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f;
+		boolean equal = 1.0f == sum;
+		System.out.println(equal);
+	}
+}
